205200811c243ff3c887e78670008f6be60834fa
shaocc <kugou2008.happy@163.com>
2017-10-17 16:57:50 +0800

    add storage interface

--- broker/storage/storage.go
+++ broker/storage/storage.go
@@ -1,1 +1,9 @@
 package storage
+
+// Storage storage
+type Storage interface {
+	Init() error                                                   // init storage server
+	Store(key string, value []byte, msgid string, ttl int64) error // insert message
+	Get(key string, limit int) ([][]byte, error)                   // get message
+	Del(key string, msgid string) error                            // delete message
+}
