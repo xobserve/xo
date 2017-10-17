0238951a79d228c396c66adc63d552c821f950a9
shaocc <kugou2008.happy@163.com>
2017-10-17 17:17:09 +0800

    add memory storage

---
+++ broker/storage/memory.go
@@ -0,0 +1,30 @@
+package storage
+
+// InMemory  a memory storage
+type InMemory struct {
+}
+
+// Init init memory server
+func (m *InMemory) Init() error {
+	return nil
+}
+
+// Store store message
+func (m *InMemory) Store(key string, value []byte, msgid string, ttl int64) error {
+	return nil
+}
+
+// Get get message
+func (m *InMemory) Get(key string, limit int) ([][]byte, error) {
+	return nil, nil
+}
+
+// Del delete message
+func (m *InMemory) Del(key string, msgid string) error {
+	return nil
+}
+
+// Name get store name
+func (m *InMemory) Name() string {
+	return "InMemory"
+}
--- broker/storage/storage.go
+++ broker/storage/storage.go
@@ -6,4 +6,5 @@ type Storage interface {
 	Store(key string, value []byte, msgid string, ttl int64) error // insert message
 	Get(key string, limit int) ([][]byte, error)                   // get message
 	Del(key string, msgid string) error                            // delete message
+	Name() string
 }
