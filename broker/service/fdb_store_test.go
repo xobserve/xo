cb4b3026b40ad59b7307ae22f3e5f07dd55bbfa5
sunface <cto@188.com>
2018-06-22 09:56:01 +0800

    add meq website

--- broker/service/fdb_store_test.go
+++ broker/service/fdb_store_test.go
@@ -13,102 +13,102 @@
 // limitations under the License.
 package service
 
-// import (
-// 	"testing"
-// 	"time"
+import (
+	"testing"
+	"time"
 
-// 	"github.com/apple/foundationdb/bindings/go/src/fdb"
-// 	"github.com/apple/foundationdb/bindings/go/src/fdb/tuple"
-// 	"github.com/cosmos-gg/meq/proto"
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/apple/foundationdb/bindings/go/src/fdb"
+	"github.com/apple/foundationdb/bindings/go/src/fdb/tuple"
+	"github.com/cosmos-gg/meq/proto"
+	"github.com/stretchr/testify/assert"
+)
 
-// var testNamespace = "test"
+var testNamespace = "test"
 
-// func TestFdbPutAndGet(t *testing.T) {
-// 	// init broker
-// 	b := NewBroker("../broker.yaml")
-// 	b.conf.Store.Engine = "fdb"
-// 	b.conf.Store.FDB.Namespace = testNamespace
-// 	b.Start()
-// 	defer b.Shutdown()
+func TestFdbPutAndGet(t *testing.T) {
+	// init broker
+	b := NewBroker("../broker.yaml")
+	b.conf.Store.Engine = "fdb"
+	b.conf.Store.FDB.Namespace = testNamespace
+	b.Start()
+	defer b.Shutdown()
 
-// 	time.Sleep(2 * time.Second)
-// 	// put into fdb
-// 	b.store.Store(mockExactMsgs)
-// 	time.Sleep(2 * time.Second)
-// 	// get from fdb
-// 	msgs := b.store.Query(mockExactMsgs[0].Topic, 20, mockExactMsgs[0].ID, false)
-// 	assert.Equal(t, mockExactMsgs[1:], msgs)
+	time.Sleep(2 * time.Second)
+	// put into fdb
+	b.store.Store(mockExactMsgs)
+	time.Sleep(2 * time.Second)
+	// get from fdb
+	msgs := b.store.Query(mockExactMsgs[0].Topic, 20, mockExactMsgs[0].ID, false)
+	assert.Equal(t, mockExactMsgs[1:], msgs)
 
-// 	f := b.store.(*FdbStore)
-// 	f.dbs[0].db.Transact(func(tr fdb.Transaction) (ret interface{}, err error) {
-// 		tr.ClearRange(f.dbs[0].msgsp)
-// 		tr.ClearRange(f.dbs[0].normalCountSP)
-// 		return
-// 	})
+	f := b.store.(*FdbStore)
+	f.dbs[0].db.Transact(func(tr fdb.Transaction) (ret interface{}, err error) {
+		tr.ClearRange(f.dbs[0].msgsp)
+		tr.ClearRange(f.dbs[0].normalCountSP)
+		return
+	})
 
-// }
+}
 
-// func TestFdbPutAndGetFromNewest(t *testing.T) {
-// 	// init broker
-// 	b := NewBroker("../broker.yaml")
-// 	b.conf.Store.Engine = "fdb"
-// 	b.conf.Store.FDB.Namespace = testNamespace
-// 	b.Start()
-// 	defer b.Shutdown()
+func TestFdbPutAndGetFromNewest(t *testing.T) {
+	// init broker
+	b := NewBroker("../broker.yaml")
+	b.conf.Store.Engine = "fdb"
+	b.conf.Store.FDB.Namespace = testNamespace
+	b.Start()
+	defer b.Shutdown()
 
-// 	time.Sleep(2 * time.Second)
-// 	// put into fdb
-// 	b.store.Store(mockExactMsgs)
+	time.Sleep(2 * time.Second)
+	// put into fdb
+	b.store.Store(mockExactMsgs)
 
-// 	time.Sleep(2 * time.Second)
-// 	// get from fdb
-// 	msgs := b.store.Query(mockExactMsgs[0].Topic, 20, proto.MSG_NEWEST_OFFSET, false)
+	time.Sleep(2 * time.Second)
+	// get from fdb
+	msgs := b.store.Query(mockExactMsgs[0].Topic, 20, proto.MSG_NEWEST_OFFSET, false)
 
-// 	assert.Equal(t, mockExactMsgs, msgs)
+	assert.Equal(t, mockExactMsgs, msgs)
 
-// 	f := b.store.(*FdbStore)
-// 	f.dbs[0].db.Transact(func(tr fdb.Transaction) (ret interface{}, err error) {
-// 		tr.ClearRange(f.dbs[0].msgsp)
-// 		tr.ClearRange(f.dbs[0].normalCountSP)
-// 		return
-// 	})
+	f := b.store.(*FdbStore)
+	f.dbs[0].db.Transact(func(tr fdb.Transaction) (ret interface{}, err error) {
+		tr.ClearRange(f.dbs[0].msgsp)
+		tr.ClearRange(f.dbs[0].normalCountSP)
+		return
+	})
 
-// }
+}
 
-// func TestFdbCount(t *testing.T) {
-// 	// init broker
-// 	b := NewBroker("../broker.yaml")
-// 	b.conf.Store.Engine = "fdb"
-// 	b.conf.Store.FDB.Namespace = testNamespace
-// 	b.Start()
-// 	defer b.Shutdown()
+func TestFdbCount(t *testing.T) {
+	// init broker
+	b := NewBroker("../broker.yaml")
+	b.conf.Store.Engine = "fdb"
+	b.conf.Store.FDB.Namespace = testNamespace
+	b.Start()
+	defer b.Shutdown()
 
-// 	topic := []byte("test")
+	topic := []byte("test")
 
-// 	time.Sleep(2 * time.Second)
-// 	f := b.store.(*FdbStore)
-// 	d := f.dbs[0]
-// 	ck := d.normalCountSP.Pack(tuple.Tuple{topic})
-// 	incrCount(d.db, ck, 10)
-// 	decrCount(d.db, ck, 3)
+	time.Sleep(2 * time.Second)
+	f := b.store.(*FdbStore)
+	d := f.dbs[0]
+	ck := d.normalCountSP.Pack(tuple.Tuple{topic})
+	incrCount(d.db, ck, 10)
+	decrCount(d.db, ck, 3)
 
-// 	n, _ := getCount(d.db, ck)
-// 	assert.Equal(t, int64(7), n)
+	n, _ := getCount(d.db, ck)
+	assert.Equal(t, int64(7), n)
 
-// 	decrCount(d.db, ck, 10)
-// 	n, _ = getCount(d.db, ck)
-// 	assert.Equal(t, int64(0), n)
+	decrCount(d.db, ck, 10)
+	n, _ = getCount(d.db, ck)
+	assert.Equal(t, int64(0), n)
 
-// 	incrCount(d.db, ck, 10)
-// 	decrCount(d.db, ck, proto.REDUCE_ALL_COUNT)
-// 	n, _ = getCount(d.db, ck)
-// 	assert.Equal(t, int64(0), n)
+	incrCount(d.db, ck, 10)
+	decrCount(d.db, ck, proto.REDUCE_ALL_COUNT)
+	n, _ = getCount(d.db, ck)
+	assert.Equal(t, int64(0), n)
 
-// 	d.db.Transact(func(tr fdb.Transaction) (ret interface{}, err error) {
-// 		tr.ClearRange(d.msgsp)
-// 		tr.ClearRange(d.normalCountSP)
-// 		return
-// 	})
-// }
+	d.db.Transact(func(tr fdb.Transaction) (ret interface{}, err error) {
+		tr.ClearRange(d.msgsp)
+		tr.ClearRange(d.normalCountSP)
+		return
+	})
+}
