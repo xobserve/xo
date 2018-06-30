c9d57a92f5b5aff44498a45216bff3e92bd18dde
sunface <cto@188.com>
2018-06-30 18:52:04 +0800

    updaate vendor

--- broker/cmd/root.go
+++ broker/cmd/root.go
@@ -22,7 +22,6 @@ import (
 
 	"github.com/meqio/meq/broker/service"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -61,7 +60,7 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
+	// cobra.OnInitialize(initConfig)
 
 	// Here you will define your flags and configuration settings.
 	// Cobra supports Persistent Flags, which, if defined here,
@@ -74,17 +73,17 @@ func init() {
 }
 
 // initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" { // enable ability to specify config file via flag
-		viper.SetConfigFile(cfgFile)
-	}
+// func initConfig() {
+// 	if cfgFile != "" { // enable ability to specify config file via flag
+// 		viper.SetConfigFile(cfgFile)
+// 	}
 
-	viper.SetConfigName(".broker") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")   // adding home directory as first search path
-	viper.AutomaticEnv()           // read in environment variables that match
+// 	viper.SetConfigName(".broker") // name of config file (without extension)
+// 	viper.AddConfigPath("$HOME")   // adding home directory as first search path
+// 	viper.AutomaticEnv()           // read in environment variables that match
 
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
-}
+// 	// If a config file is found, read it in.
+// 	if err := viper.ReadInConfig(); err == nil {
+// 		fmt.Println("Using config file:", viper.ConfigFileUsed())
+// 	}
+// }
