523e20720b87b8498a561608d40bd6601e6da439
sunface <cto@188.com>
2022-09-06 18:37:41 +0800

    remove unused variables

--- ui/src/components/back-to-top.tsx
+++ ui/src/components/back-to-top.tsx
@@ -1,5 +1,4 @@
-import { Box, Image } from "@chakra-ui/react";
-import Link from "next/link";
+import { Box } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import SvgButton from "./icons/SvgButton";
 
