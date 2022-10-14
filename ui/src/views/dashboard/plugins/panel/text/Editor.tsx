369cab955073743a52812d196694f33d86bb7f4c
sunface <cto@188.com>
2022-10-14 14:19:01 +0800

    remove unused variables

--- ui/src/views/dashboard/plugins/panel/text/Editor.tsx
+++ ui/src/views/dashboard/plugins/panel/text/Editor.tsx
@@ -1,4 +1,4 @@
-import { Input, Textarea } from "@chakra-ui/react"
+import { Textarea } from "@chakra-ui/react"
 import PanelAccordion from "src/views/dashboard/edit-panel/Accordion"
 import PanelEditItem from "src/views/dashboard/edit-panel/PanelEditItem"
 import { cloneDeep } from "lodash"
