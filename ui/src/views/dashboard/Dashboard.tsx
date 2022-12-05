803122ddf4a510d62fd4598ada23f61bed07de0c
sunface <cto@188.com>
2022-12-05 14:41:36 +0800

    add largescreen component border box8

--- ui/src/views/dashboard/Dashboard.tsx
+++ ui/src/views/dashboard/Dashboard.tsx
@@ -18,7 +18,6 @@ import { TimeChangedEvent, VariableChangedEvent } from "src/data/bus-events"
 import { useImmer } from "use-immer"
 import { setAutoFreeze } from "immer";
 import { initPanelSettings } from "./plugins/panel/initSettings"
-import BorderBox7 from "components/largescreen/border/Border7"
 import BorderBox8 from "components/largescreen/border/Border8"
  
 
