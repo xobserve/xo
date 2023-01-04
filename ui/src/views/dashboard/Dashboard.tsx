44b9436a613007a40cd74853e111f5e5b0b42b13
sunface <cto@188.com>
2023-01-04 12:36:13 +0800

    remove unused variables

--- ui/src/views/dashboard/Dashboard.tsx
+++ ui/src/views/dashboard/Dashboard.tsx
@@ -18,7 +18,6 @@ import { TimeChangedEvent, VariableChangedEvent } from "src/data/bus-events"
 import { useImmer } from "use-immer"
 import { setAutoFreeze } from "immer";
 import { initPanelSettings } from "./plugins/panel/initSettings"
-import BorderBox6 from "components/largescreen/border/Border6"
 import BorderBox7 from "components/largescreen/border/Border7"
  
 
