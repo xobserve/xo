41be251e66d8476a8170633e91db5c68d24bcd0c
sunface <cto@188.com>
2023-01-15 15:26:07 +0800

    add largescreen component border box5

---
+++ ui/src/components/largescreen/border/Border5.tsx
@@ -0,0 +1,115 @@
+import React, { useMemo, forwardRef } from 'react'
+
+
+import classnames from 'classnames'
+
+import { deepMerge } from '@jiaminghi/charts/lib/util/index'
+import { deepClone } from '@jiaminghi/c-render/lib/plugin/util'
+
+import useAutoResize from '../autoResize'
+
+import { BorderBoxProps } from '../types'
+import { Box } from '@chakra-ui/react'
+
+const defaultColor = ['rgba(255, 255, 255, 0.35)', 'rgba(255, 255, 255, 0.20)']
+
+const BorderBox5 = forwardRef(({ children, reverse = false, className, style, color = [], backgroundColor = 'transparent' }:BorderBoxProps, ref) => {
+  const { width, height, domRef } = useAutoResize(ref)
+
+  const mergedColor = useMemo(() => deepMerge(deepClone(defaultColor, true), color || []), [color])
+
+  const classNames = useMemo(() => classnames('dv-border-box-5', className), [
+    className
+  ])
+
+  return (
+    <Box className={classNames} style={style} ref={domRef} sx={cssStyles}>
+      <svg
+        className={`dv-border-svg-container  ${reverse && 'dv-reverse'}`}
+        width={width}
+        height={height}
+      >
+        <polygon fill={backgroundColor} points={`
+          10, 22 ${width - 22}, 22 ${width - 22}, ${height - 86} ${width - 84}, ${height - 24} 10, ${height - 24}
+        `} />
+        <polyline
+          className='dv-bb5-line-1'
+          stroke={mergedColor[0]}
+          points={`8, 5 ${width - 5}, 5 ${width - 5}, ${height - 100}
+          ${width - 100}, ${height - 5} 8, ${height - 5} 8, 5`}
+        />
+        <polyline
+          className='dv-bb5-line-2'
+          stroke={mergedColor[1]}
+          points={`3, 5 ${width - 20}, 5 ${width - 20}, ${height - 60}
+          ${width - 74}, ${height - 5} 3, ${height - 5} 3, 5`}
+        />
+        <polyline
+          className='dv-bb5-line-3'
+          stroke={mergedColor[1]}
+          points={`50, 13 ${width - 35}, 13`}
+        />
+        <polyline
+          className='dv-bb5-line-4'
+          stroke={mergedColor[1]}
+          points={`15, 20 ${width - 35}, 20`}
+        />
+        <polyline
+          className='dv-bb5-line-5'
+          stroke={mergedColor[1]}
+          points={`15, ${height - 20} ${width - 110}, ${height - 20}`}
+        />
+        <polyline
+          className='dv-bb5-line-6'
+          stroke={mergedColor[1]}
+          points={`15, ${height - 13} ${width - 110}, ${height - 13}`}
+        />
+      </svg>
+
+      <div className='border-box-content'>{children}</div>
+    </Box>
+  )
+})
+
+
+export default BorderBox5
+
+const cssStyles =  {
+    position: 'relative',
+    width: '100%',
+    height: "100%",
+  
+    ".dv-reverse": {
+      transform: 'rotate(180deg)'
+    },
+  
+    '.dv-border-svg-container': {
+      position: 'absolute',
+      top: '0px',
+      left: '0px',
+      width: '100%',
+      height: "100%",
+  
+      '& > polyline': {
+        fill: 'none'
+      }
+    },
+  
+    '.dv-bb5-line-1, .dv-bb5-line-2': {
+      'stroke-width': 1
+    },
+  
+    '.dv-bb5-line-3, .dv-bb5-line-6': {
+      'stroke-width': 5
+    },
+  
+    '.dv-bb5-line-4, .dv-bb5-line-5': {
+      'stroke-width': 2
+    },
+  
+    '.border-box-content': {
+      position: 'relative',
+      width: '100%',
+      height: '100%',
+    }
+  }
