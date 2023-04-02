8b05ae859d9eba31c3f5788096479a51ec000e22
sunface <cto@188.com>
2023-04-02 15:37:25 +0800

    add largescreen/decoration5

---
+++ ui/src/components/largescreen/decoration/Decoration5.tsx
@@ -0,0 +1,103 @@
+import React, { useMemo, forwardRef } from 'react'
+
+
+import classnames from 'classnames'
+
+import { getPolylineLength, deepMerge } from '@jiaminghi/charts/lib/util'
+
+import { deepClone } from '@jiaminghi/c-render/lib/plugin/util'
+
+import useAutoResize from '../autoResize'
+
+import { DecorationProps1 } from '../types'
+import { Box } from '@chakra-ui/react'
+
+const defaultColor = ['#3f96a5', '#3f96a5']
+
+const Decoration5 = forwardRef(({ className, dur = 1.2, style, color = [] }:DecorationProps1, ref) => {
+  const { width, height, domRef } = useAutoResize(ref)
+
+  function calcSVGData() {
+    let line1Points = [
+      [0, height * 0.2],
+      [width * 0.18, height * 0.2],
+      [width * 0.2, height * 0.4],
+      [width * 0.25, height * 0.4],
+      [width * 0.27, height * 0.6],
+      [width * 0.72, height * 0.6],
+      [width * 0.75, height * 0.4],
+      [width * 0.8, height * 0.4],
+      [width * 0.82, height * 0.2],
+      [width, height * 0.2]
+    ] as any
+
+    let line2Points = [[width * 0.3, height * 0.8], [width * 0.7, height * 0.8]] as any
+
+    const line1Length = getPolylineLength(line1Points)
+    const line2Length = getPolylineLength(line2Points)
+
+    line1Points = line1Points.map(point => point.join(',')).join(' ')
+    line2Points = line2Points.map(point => point.join(',')).join(' ')
+
+    return { line1Points, line2Points, line1Length, line2Length }
+  }
+
+  const mergedColor = useMemo(() => deepMerge(deepClone(defaultColor, true), color || []), [color])
+
+  const { line1Points, line2Points, line1Length, line2Length } = useMemo(
+    calcSVGData,
+    [width, height]
+  )
+
+  const classNames = useMemo(() => classnames('dv-decoration-5', className), [
+    className
+  ])
+
+  return (
+    <Box className={classNames} style={style} ref={domRef} width="100%" height="100%">
+      <svg width={width} height={height}>
+        <polyline
+          fill='transparent'
+          stroke={mergedColor[0]}
+          strokeWidth='3'
+          points={line1Points}
+        >
+          <animate
+            attributeName='stroke-dasharray'
+            attributeType='XML'
+            from={`0, ${line1Length / 2}, 0, ${line1Length / 2}`}
+            to={`0, 0, ${line1Length}, 0`}
+            dur={`${dur}s`}
+            begin='0s'
+            calcMode='spline'
+            keyTimes='0;1'
+            keySplines='0.4,1,0.49,0.98'
+            repeatCount='indefinite'
+          />
+        </polyline>
+        <polyline
+          fill='transparent'
+          stroke={mergedColor[1]}
+          strokeWidth='2'
+          points={line2Points}
+        >
+          <animate
+            attributeName='stroke-dasharray'
+            attributeType='XML'
+            from={`0, ${line2Length / 2}, 0, ${line2Length / 2}`}
+            to={`0, 0, ${line2Length}, 0`}
+            dur={`${dur}s`}
+            begin='0s'
+            calcMode='spline'
+            keyTimes='0;1'
+            keySplines='.4,1,.49,.98'
+            repeatCount='indefinite'
+          />
+        </polyline>
+      </svg>
+    </Box>
+  )
+})
+
+
+export default Decoration5
--- ui/src/views/dashboard/Dashboard.tsx
+++ ui/src/views/dashboard/Dashboard.tsx
@@ -18,7 +18,7 @@ import { TimeChangedEvent, VariableChangedEvent } from "src/data/bus-events"
 import { useImmer } from "use-immer"
 import { setAutoFreeze } from "immer";
 import { initPanelSettings } from "./plugins/panel/initSettings"
-import Decoration4 from "components/largescreen/decoration/Decoration4"
+import Decoration5 from "components/largescreen/decoration/Decoration5"
  
 
 
@@ -137,10 +137,7 @@ const DashboardWrapper = ({dashboardId}) => {
         <>
             <PageContainer fullscreen={fullscreen} bg={dashboard?.data.styles?.bg}>
                 {dashboard && <Box pl="6px" pr="6px" width="100%">
-                    <Decoration4 reverse  style={{
-                        width: '150px',
-                        height: '5px'
-                    }}/>
+                    <Decoration5 />
                     <DashboardHeader fullscreen={fullscreen} onFullscreenChange={onFullscreenChange} dashboard={dashboard} onTimeChange={t => {dispatch({type:  TimeChangedEvent,data: t});setTimeRange(t)}} timeRange={timeRange}  onChange={onDashbardChange} />
                     <Box mt={fullscreen ? 0 : (visibleVars?.length > 0 ? "67px" : "38px")} py="2">
                         {dashboard.data.panels?.length > 0 && <DashboardGrid dashboard={dashboard} onChange={onDashbardChange} />}
