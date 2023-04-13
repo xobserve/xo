066e512ed53c992f4a61ce41780f2c3168f91adc
sunface <cto@188.com>
2023-04-13 20:54:56 +0800

    add a invisible border for panel to avoid pixel moving

--- ui/src/views/dashboard/grid/PanelGrid.tsx
+++ ui/src/views/dashboard/grid/PanelGrid.tsx
@@ -193,7 +193,7 @@ export const PanelComponent = ({ dashboard, panel, onRemovePanel, width, height,
 
 
     console.log("panel component rendered, data: ", panelData, panel)
-    return <Box height="100%" className="hover-bordered">
+    return <Box height="100%" className="hover-bordered" border={`1px solid transparent`}>
         <PanelHeader panel={panel} data={panelData} queryError={queryError} onCopyPanel={onCopyPanel} onRemovePanel={onRemovePanel} />
         {panelData && <Box
             // panel={panel}
