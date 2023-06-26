f18b4a790a9cf28625c901e49e696fc7bd1f7a82
sunface <cto@188.com>
2023-06-26 10:17:55 +0800

    feat: add legend to Stat panel

--- ui/src/views/dashboard/plugins/panel/stat/Stat.tsx
+++ ui/src/views/dashboard/plugins/panel/stat/Stat.tsx
@@ -8,7 +8,7 @@ import { parseOptions } from './options';
 import { isEmpty } from "lodash";
 
 import Tooltip from "./Tooltip";
-import { Box, Center, Text, useColorMode } from "@chakra-ui/react";
+import { Box, Center, Flex, Text, useColorMode,Tooltip as ChakraTooltip } from "@chakra-ui/react";
 import { parseLegendFormat } from "utils/format";
 import { replaceWithVariables } from "utils/variable";
 import { variables } from "src/views/dashboard/Dashboard";
@@ -26,7 +26,7 @@ const StatPanel = memo((props: StatPanelProps) => {
         return (<Center height="100%">No data</Center>)
     }
 
-    const [data,value] = useMemo(() => {
+    const [data, value] = useMemo(() => {
         const res = []
         let value;
         if (props.data.length > 0) {
@@ -35,23 +35,25 @@ const StatPanel = memo((props: StatPanelProps) => {
                 value = props.data[0].value
             }
         }
-        
+
         return [res, value]
     }, [props.data])
 
 
     const { colorMode } = useColorMode()
 
-    const options = useMemo(() => {
+    const [options, legend] = useMemo(() => {
         let o;
+        let legend;
         // transform series name based on legend format 
         const ds = props.panel.datasource
         for (const query of ds.queries) {
-            if (!isEmpty(query.legend)) {
-                const formats = parseLegendFormat(query.legend)
+            // if (!isEmpty(query.legend)) {
+            const formats = parseLegendFormat(query.legend)
 
-                data.map(frame => {
-                    if (frame.id == query.id) {
+            data.map(frame => {
+                if (frame.id == query.id) {
+                    if (!isEmpty(query.legend)) {
                         frame.name = query.legend
                         if (!isEmpty(formats)) {
                             for (const format of formats) {
@@ -65,13 +67,14 @@ const StatPanel = memo((props: StatPanelProps) => {
                         // replace ${xxx} format with corresponding variables
                         frame.name = replaceWithVariables(frame.name, variables)
                     }
-                })
-            }
+                    legend = frame.name
+                }
+            })
         }
 
         o = parseOptions(props, data)
 
-        return o
+        return [o, legend]
     }, [props.panel, props.data, colorMode])
 
     const [uplot, setUplot] = useState<uPlot>(null)
@@ -79,14 +82,18 @@ const StatPanel = memo((props: StatPanelProps) => {
     const onChartCreate = useCallback((chart) => { setUplot((chart)); props.sync?.sub(chart) }, [props.sync])
 
 
+    console.log("here3333legend:", legend)
     return (
         <>
             <Box h="100%" className="panel-graph">
 
                 {props.panel.plugins.stat.styles.graphHeight < 100 && <Box height={`${100 - props.panel.plugins.stat.styles.graphHeight}%`}>
-                    {!isEmpty(data) && 
+                    {!isEmpty(data) &&
                         <Center height="100%">
-                            <Text fontSize="50" color={props.panel.plugins.stat.styles.color} fontWeight="bold">{formatUnit(value, props.panel.plugins.stat.units, props.panel.plugins.stat.decimal)}</Text>
+                            <Flex width="100%" px={4} justifyContent={props.panel.plugins.stat.showLegend ? "space-between" : "center"} fontSize="50" color={props.panel.plugins.stat.styles.color} fontWeight="bold">
+                                {props.panel.plugins.stat.showLegend && <ChakraTooltip label={legend}><Text maxWidth="50%" noOfLines={1}>{legend}</Text></ChakraTooltip>}
+                                <Text >{formatUnit(value, props.panel.plugins.stat.units, props.panel.plugins.stat.decimal)}</Text>
+                            </Flex>
                         </Center>}
                 </Box>}
                 <Box>
