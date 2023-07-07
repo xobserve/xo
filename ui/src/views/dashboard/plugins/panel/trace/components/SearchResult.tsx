4c5c0ea0670c5450c3885862e96a3676d5452949
sunface <cto@188.com>
2023-07-07 16:18:48 +0800

    feat: trigger trace compare

--- ui/src/views/dashboard/plugins/panel/trace/components/SearchResult.tsx
+++ ui/src/views/dashboard/plugins/panel/trace/components/SearchResult.tsx
@@ -2,10 +2,12 @@ import { Panel } from "types/dashboard"
 import { Trace } from "types/plugins/trace"
 import SearchResultPlot from "./SearchResultPlot"
 import { TimeRange } from "types/time"
-import { Box, Button, Flex, HStack, Select, Text, VStack } from "@chakra-ui/react"
+import { Box, Button, Flex, HStack, Popover, PopoverArrow, PopoverBody, PopoverContent, PopoverTrigger, Select, StackDivider, Text, VStack, chakra } from "@chakra-ui/react"
 import TraceCard from "./TraceCard"
 import { useEffect, useMemo, useState } from "react"
-import { clone, cloneDeep } from "lodash"
+import { clone, cloneDeep, remove } from "lodash"
+import TraceCompare from "./TraceCompare"
+import { FaTimes } from "react-icons/fa"
 
 interface Props {
     panel: Panel
@@ -15,10 +17,11 @@ interface Props {
 const TraceSearchResult = ({ panel, traces, timeRange }: Props) => {
     const [selectedTraces, setSelectedTraces] = useState<Trace[]>([])
     const [sort, setSort] = useState(traceSortTypes[0].value)
+    const [comparison, setComparison] = useState<string[]>([])
 
     useEffect(() => {
         setSelectedTraces(clone(traces))
-    },[traces])
+    }, [traces])
 
     const maxDuration = useMemo(() => Math.max(...traces.map(trace => trace.duration)), [traces])
 
@@ -44,16 +47,43 @@ const TraceSearchResult = ({ panel, traces, timeRange }: Props) => {
             default:
                 return traces
         }
-    },[selectedTraces,sort])
+    }, [selectedTraces, sort])
+
+    const onTraceChecked = traceId => {
+
+        if (comparison.includes(traceId)) {
+            remove(comparison, i => i == traceId)
+        } else {
+            comparison.push(traceId)
+        }
+
+        setComparison([...comparison])
+    }
+
+    const comparedTraces = comparison.map(traceId => {
+        return traces.find(t => t.traceID == traceId)
+    })
+
+    const removeFromCompare = traceId => {
+        remove(comparison, i => i == traceId)
+        setComparison([...comparison])
+    }
+
 
     return (<Box pl="2">
         <SearchResultPlot traces={traces} timeRange={timeRange} onSelect={onSelect} />
         <Box pl="2" pr="20px">
             <Flex alignItems="center" justifyContent="space-between" mb="1">
                 <HStack height="40px" textStyle="title">
-                {selectedTraces.length != traces.length ? <Text>{selectedTraces.length} Traces Selected</Text> : <Text>{selectedTraces.length} Traces Total</Text>}
-                {selectedTraces.length != traces.length && <Button size="sm" variant="outline" onClick={() => setSelectedTraces(clone(traces))}>Clear selection</Button>}
+                    {selectedTraces.length != traces.length ? <Text>{selectedTraces.length} Traces Selected</Text> : <Text>{selectedTraces.length} Traces Total</Text>}
+                    {selectedTraces.length != traces.length && <Button size="sm" variant="outline" onClick={() => setSelectedTraces(clone(traces))}>Clear selection</Button>}
                 </HStack>
+                {
+                    comparison.length > 0 && <HStack textStyle="title">
+                        <Text><ComparisonTraces removeFromCompare={removeFromCompare} comparedTraces={comparedTraces} maxDuration={maxDuration}/>  selected for comparison</Text>
+                        <TraceCompare traces={comparedTraces}/>
+                    </HStack>
+                }
                 <HStack alignItems="center">
                     <Text minWidth="fit-content">Sort</Text>
                     <Select size="sm" value={sort} onChange={e => setSort(e.currentTarget.value)}>
@@ -61,8 +91,8 @@ const TraceSearchResult = ({ panel, traces, timeRange }: Props) => {
                     </Select>
                 </HStack>
             </Flex>
-            <VStack alignItems="left">
-                {sortedTraces.map(trace => <TraceCard key={trace.traceID} trace={trace} maxDuration={maxDuration} />)}
+            <VStack alignItems="left" maxH="500px" overflowY="scroll">
+                {sortedTraces.map(trace => <TraceCard key={trace.traceID} trace={trace} maxDuration={maxDuration} checked={comparison.includes(trace.traceID)} checkDisabled={comparison.length >= 2 && !comparison.includes(trace.traceID)} onChecked={onTraceChecked} />)}
             </VStack>
         </Box>
     </Box>)
@@ -71,6 +101,26 @@ const TraceSearchResult = ({ panel, traces, timeRange }: Props) => {
 export default TraceSearchResult
 
 
+const ComparisonTraces = ({ comparedTraces,maxDuration,removeFromCompare }: { comparedTraces: Trace[],maxDuration: number,removeFromCompare:any }) => {
+    return (<>
+        <Popover trigger="hover" placement="left">
+            <PopoverTrigger>
+                <chakra.span color="brand.500" cursor="pointer">{comparedTraces.length}</chakra.span>
+            </PopoverTrigger>
+            <PopoverContent minWidth="500px">
+                <PopoverArrow />
+                <PopoverBody>
+                    <VStack alignItems="left" divider={<StackDivider />}>
+                        {comparedTraces.map(trace => {return trace && <HStack spacing={2}>
+                        <TraceCard  trace={trace} maxDuration={maxDuration} simple/>
+                        <FaTimes cursor="pointer" onClick={() => removeFromCompare(trace.traceID)}/>
+                        </HStack>})}
+                    </VStack>
+                </PopoverBody>
+            </PopoverContent>
+        </Popover>
+    </>)
+}
 const traceSortTypes = [
     {
         label: "Most Recent",
@@ -88,7 +138,7 @@ const traceSortTypes = [
         label: "Shortest Duruation",
         value: "shortest"
     },
-    {   
+    {
         label: "Most Spans",
         value: "mostSpans"
     },
--- ui/src/views/dashboard/plugins/panel/trace/components/TraceCard.tsx
+++ ui/src/views/dashboard/plugins/panel/trace/components/TraceCard.tsx
@@ -1,8 +1,6 @@
-import { Box, Checkbox, Divider, Flex, HStack, Tag, TagLabel, TagLeftIcon, TagRightIcon, Text, Wrap, WrapItem, useColorModeValue } from "@chakra-ui/react"
+import { Box, Checkbox, Flex, HStack, Tag, TagLabel, TagLeftIcon, Text, Wrap, WrapItem, useColorModeValue } from "@chakra-ui/react"
 import { Trace } from "types/plugins/trace"
 import { formatDuration, formatRelativeDate } from "../utils/date"
-import { useMemo } from "react"
-import { isErrorTag } from "../utils/trace"
 import { sortBy } from "lodash"
 import colorGenerator from "utils/colorGenerator"
 import { FaInfoCircle } from "react-icons/fa"
@@ -11,9 +9,13 @@ import moment from "moment"
 interface Props {
     trace: Trace
     maxDuration: number
+    checked?: boolean
+    onChecked?: (traceId: string) => void
+    checkDisabled?: boolean
+    simple?: boolean
 }
 
-const TraceCard = ({ trace, maxDuration }: Props) => {
+const TraceCard = ({ trace, maxDuration,checked=false, onChecked=null,simple=false,checkDisabled=false }: Props) => {
     const mDate = moment(trace.startTime / 1000);
     const timeStr = mDate.format('h:mm:ss a')
 
@@ -23,9 +25,9 @@ const TraceCard = ({ trace, maxDuration }: Props) => {
     return (<Box width="100%" className="bordered" borderRadius="0" cursor="pointer" onClick={onTraceClick}>
         <Box width="100%" position="relative" className="label-bg">
             <HStack spacing={0} py="5px">
-                <Flex alignItems="center" px="2" zIndex={2}  onClick={e => {
-                    e.stopPropagation();
-                }}><Checkbox bg={useColorModeValue("white", "transparent")}/></Flex>
+                {onChecked && <Flex alignItems="center" px="2" zIndex={2}  onClick={e => {
+                    e.stopPropagation(); 
+                }}><Checkbox defaultChecked={checked} isChecked={checked} isDisabled={checkDisabled} bg={useColorModeValue("white", "transparent")} onChange={e =>  onChecked(trace.traceID)  }/></Flex>}
                 <Box width={`${(trace.duration / maxDuration) * 100}%`} bg={useColorModeValue("#d7e7ea", "brand.800")} height="100%" position="absolute" top="0"></Box>
                 <Flex alignItems="center" width="100%" justifyContent="space-between" position="relative" pr="2" >
                     <HStack>
@@ -44,7 +46,7 @@ const TraceCard = ({ trace, maxDuration }: Props) => {
                     {trace.errorsCount > 0 && <Tag size="md" variant="subtle" colorScheme="red" className="bordered" fontSize="0.8rem">{trace.errorsCount} Errors</Tag>}
                   
                 </HStack>
-                <Wrap mt="4" spacingX={3}>
+                {!simple && <Wrap mt="4" spacingX={3}>
                     {sortBy(trace.services, s => s.name).map(service => {
                         const { name, numberOfSpans: count } = service;
                         return (
@@ -65,14 +67,14 @@ const TraceCard = ({ trace, maxDuration }: Props) => {
                             </WrapItem>
                         );
                     })}
-                </Wrap>
+                </Wrap>}
             </Box>
 
             <Box fontSize="0.85rem">
-                <HStack spacing={1}>
+                {!simple && <HStack spacing={1}>
                     <Text className="bordered-right" pr="1">{formatRelativeDate(trace.startTime / 1000)}</Text>
                     <Text>{timeStr}</Text>
-                </HStack>
+                </HStack>}
                 <Text>{mDate.fromNow()}</Text>
             </Box>
         </Flex>
