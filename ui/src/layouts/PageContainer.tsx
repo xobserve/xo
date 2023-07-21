fcf141f8c258a5e9635253fd61281d9ea0b5d215
sunface <cto@188.com>
2023-07-22 02:48:04 +0800

    feat: to avaid sidemenu width sliding, wo make loading sidemenu happened before rendering

--- ui/src/layouts/PageContainer.tsx
+++ ui/src/layouts/PageContainer.tsx
@@ -1,7 +1,21 @@
-import { Box, Flex, HStack, Text, IconButton, useColorModeValue } from "@chakra-ui/react"
+import {
+  Box,
+  Flex,
+  HStack,
+  Text,
+  IconButton,
+  useColorModeValue,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  VStack,
+  Portal,
+  Divider,
+} from "@chakra-ui/react"
 import Logo from "components/Logo"
 import React, { useEffect, useState } from "react"
-import { FaAppStore, FaGithub, FaMinus, FaPlus, FaSave, FaServer, FaTimes, FaUser } from "react-icons/fa"
 import { measureText } from "utils/measureText"
 import * as Icons from "react-icons/fa"
 import { concat, max } from "lodash"
@@ -12,7 +26,12 @@ import { commonMsg, sidebarMsg } from "src/i18n/locales/en"
 import useSession from "hooks/use-session"
 import useFullscreen from "hooks/useFullscreen"
 import { Route } from "types/route"
-import { navLinks } from "src/data/nav-links"
+import { Link, useLocation } from "react-router-dom"
+import storage from "utils/localStorage"
+import { SidemenuMinimodeKey } from "src/data/storage-keys"
+import ReserveUrls from "src/data/reserve-urls"
+import UserMenu from "components/user/UserMenu"
+import { config } from "src/data/configs/config"
 
 const miniWidth = 60
 const navSize = 15
@@ -20,17 +39,13 @@ const maxNavSize = 250
 
 interface Props {
   children: any
+  sidemenu: Route[]
   bg?: string
 }
 
-const PageContainer = ({ children, bg }: Props) => {
-  const t = useStore(commonMsg)
-  const t1 = useStore(sidebarMsg)
+const PageContainerWrapper = (props) => {
   const { session } = useSession()
-
-  const [miniMode, setMiniMode] = useState(false)
-  const [sidemenu, setSidemenu] = useState<Route[]>([])
-  const fullscreen = useFullscreen()
+  const [sidemenu, setSidemenu] = useState<Route[]>(null)
   useEffect(() => {
     if (session) {
       loadSidemenu()
@@ -42,12 +57,29 @@ const PageContainer = ({ children, bg }: Props) => {
     setSidemenu(res.data.data)
   }
 
+  return (sidemenu &&  <PageContainer {...props} sidemenu={sidemenu} />)
+}
+
+export default PageContainerWrapper
+const PageContainer = ({ children, bg, sidemenu }: Props) => {
+  const { pathname: asPath } = useLocation()
+  const t = useStore(commonMsg)
+  const t1 = useStore(sidebarMsg)
+
+
+  const [miniMode, setMiniMode] = useState(storage.get(SidemenuMinimodeKey) ?? true)
+  const fullscreen = useFullscreen()
+
+
+  const onMinimodeChange = () => {
+    setMiniMode(!miniMode);
+    storage.set(SidemenuMinimodeKey, !miniMode)
+  }
+
   const bottomNavs = [
-    { title: "Github", icon: "FaGithub", url: "" },
-    { title: "用户设置", icon: "FaUser", url: "" },
+    { title: "Github", icon: "FaGithub", url: config.repoUrl },
   ]
 
-  // 由于非 minimode 会展示子菜单，因此我们需要计算最大菜单宽度
   const childMarginLeft = 10
   let navWidth = 0
   const paddingLeft = 8
@@ -71,14 +103,15 @@ const PageContainer = ({ children, bg }: Props) => {
       }
     })
   }
+
   if (navWidth > maxNavSize) {
     navWidth = maxNavSize
   }
-
   const sideWidth = fullscreen ? 0 : (miniMode ? miniWidth : navWidth)
   const textColor = useColorModeValue("gray.500", "whiteAlpha.800")
+
   return (
-    <HStack id="main" width="100%" alignItems="top">
+    <HStack id="page-container" width="100%" alignItems="top">
       <Flex
         display={fullscreen ? "none" : "flex"}
         flexDir="column" pl={paddingLeft + 'px'}
@@ -96,40 +129,73 @@ const PageContainer = ({ children, bg }: Props) => {
       >
         <Flex id="sidemenu-top" flexDir="column" alignItems={miniMode ? "center" : "left"}>
           {miniMode ?
-            <Box cursor="pointer" onClick={() => setMiniMode(!miniMode)} mt="2"><Logo /></Box>
+            <Box cursor="pointer" onClick={onMinimodeChange} mt="2"><Logo /></Box>
             :
-            <Box cursor="pointer" onClick={() => setMiniMode(!miniMode)} opacity="0.2" position="absolute" right="1px" top="12px" className="hover-text" p="1"><Icons.FaChevronLeft /></Box>
+            <Box cursor="pointer" onClick={onMinimodeChange} opacity="0.2" position="absolute" right="1px" top="12px" className="hover-text" p="1"><Icons.FaChevronLeft /></Box>
           }
-          {sidemenu.map((nav, index) => {
-            return <Box mt="3">
-              <NavItem key={index} text={nav.title} icon={nav.icon} miniMode={miniMode} fontWeight={500} fontSize={navSize} />
+          {sidemenu.map((link, index) => {
+            return <Box mt={miniMode ? 2 : 3}>
+              <Popover trigger={miniMode ? "hover" : "click"} placement="right-start">
+                <PopoverTrigger>
+                  <Box>
+                    <NavItem isActive={miniMode ? asPath.startsWith(link.url) : asPath == link.url} key={index} text={link.title} icon={link.icon} miniMode={miniMode} fontWeight={500} url={link.children?.length > 0 ? link.children[0].url : link.url} />
+                  </Box>
+                </PopoverTrigger>
+                <Portal>
+                  <PopoverContent width="fit-content" minWidth="120px" pl="1">
+                    <PopoverHeader borderBottomWidth={link.children?.length > 0 ? '1px' : '0px'}>
+                      <Link to={link.children?.length > 0 ? link.children[0].url : link.url}>{link.title}</Link>
+                    </PopoverHeader>
+                    {link.children?.length > 0 && <PopoverBody pt="3">
+                      <VStack alignItems="left" spacing="3">
+                        {link.children.map(subLink =>
+                          <Link to={subLink.url} key={subLink.url}>
+                            <Text color={asPath == subLink.url ? useColorModeValue("brand.500", "brand.200") : textColor}>{subLink.title}</Text>
+                          </Link>
+                        )}
+                      </VStack>
+                    </PopoverBody>}
+                  </PopoverContent>
+                </Portal>
+              </Popover>
+
               {
-                !miniMode && nav.children && nav.children.map((child, index) => {
-                  return <Box mt="6px" ml={childMarginLeft + 'px'}><NavItem key={index} text={child.title} miniMode={miniMode} fontSize={navSize - 1} /></Box>
+                !miniMode && link.children && link.children.map((child, index) => {
+                  return <Box mt="6px" ml={childMarginLeft + 'px'}><NavItem isActive={asPath == child.url} key={index} text={child.title} miniMode={miniMode} fontSize={navSize - 0.5} url={child.url} /></Box>
                 })
               }
             </Box>
           })}
         </Flex>
-        <Flex id="sidemenu-bottom" flexDir="column" pb="2" alignItems={miniMode ? "center" : "left"}>
-          <Box color={textColor}><ColorModeSwitcher miniMode={miniMode} /></Box>
-          {bottomNavs.map((nav, index) => {
-            return <Box mt="3"><NavItem key={index} text={nav.title} icon={nav.icon} miniMode={miniMode} /></Box>
-          })}
+        <Flex id="sidemenu-bottom" flexDir="column" pb="2" alignItems={miniMode ? "center" : "left"} color={textColor}>
+          <VStack alignItems="left" spacing={miniMode ? 1 : 3}>
+            <NavItem isActive={asPath.startsWith(ReserveUrls.New)} url={`${ReserveUrls.New}/dashboard`} icon="FaPlus" text={t.new} miniMode={miniMode} />
+            <NavItem isActive={asPath.startsWith(ReserveUrls.Config)} url={`${ReserveUrls.Config}/datasources`} icon="FaCog" text={t.configuration} miniMode={miniMode} />
+            <NavItem isActive={asPath.startsWith(ReserveUrls.Alerts)} url={`${ReserveUrls.Alerts}`} icon="FaBell" text={t.alert} miniMode={miniMode} />
+            <NavItem isActive={asPath.startsWith(ReserveUrls.Search)} url={`${ReserveUrls.Search}`} icon="FaSearch" text={t1.search} miniMode={miniMode} />
+
+            <Divider />
+            <Box color={textColor}><ColorModeSwitcher miniMode={miniMode} /></Box>
+            {bottomNavs.map((nav, index) => {
+              return <Box><NavItem key={index} text={nav.title} icon={nav.icon} miniMode={miniMode} url={nav.url} /></Box>
+            })}
+            <UserMenu miniMode={miniMode} />
+          </VStack>
+
         </Flex>
       </Flex>
-      <Box id="container" transition="all 0.2s" width={`calc(100% - ${sideWidth}px)`} ml={sideWidth + 'px'}>
+      <Box id="main-container" transition="all 0.2s" width={`calc(100% - ${sideWidth}px)`} ml={sideWidth + 'px'}>
         {React.cloneElement(children, { sideWidth })}
       </Box>
     </HStack>)
 }
 
-export default PageContainer
 
-const NavItem = ({ text, icon = null, miniMode, fontWeight = 400, fontSize = 16 }) => {
+
+const NavItem = ({ text, icon = null, miniMode, fontWeight = 400, fontSize = navSize, url, isActive = false }) => {
   const Icon = Icons[icon]
-  return (<>
-    <HStack color={useColorModeValue("gray.500", "whiteAlpha.800")} className="hover-text" cursor="pointer">
+  return (<Link to={url}>
+    <HStack color={isActive ? useColorModeValue("brand.500", "brand.200") : useColorModeValue("gray.500", "whiteAlpha.800")} className="hover-text" cursor="pointer">
       {icon && <Box>
         {miniMode ?
           <IconButton fontSize={"1.2rem"} aria-label="" variant="ghost" color="current" _focus={{ border: null }} icon={<Icon />} />
@@ -138,5 +204,5 @@ const NavItem = ({ text, icon = null, miniMode, fontWeight = 400, fontSize = 16 
       </Box>}
       {!miniMode && <Text fontSize={`${fontSize}px`} fontWeight={fontWeight} >{text}</Text>}
     </HStack>
-  </>)
+  </Link>)
 }
