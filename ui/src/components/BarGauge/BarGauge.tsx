7f1f2b3e2e4f14327522cc41441a01880367e48a
sunface <cto@188.com>
2023-07-21 15:43:32 +0800

    perf: update BarGauge lcd mode styles

--- ui/src/components/BarGauge/BarGauge.tsx
+++ ui/src/components/BarGauge/BarGauge.tsx
@@ -52,6 +52,7 @@ export interface BarGaugeValue {
 
 const lcdCellWidth = 12
 const lcdCellSpacing = 2
+const minMaxHeight = 15
 const BarGauge = (props: Props) => {
     const { data, width, height, orientation = "horizontal", mode = "basic", titleSize = 18, textSize = 16, borderRadius = "4px", showUnfilled = true, fillOpacity = 0.6, showMax = false, showMin = false } = props
     const Stack = orientation == "horizontal" ? VStack : HStack
@@ -66,7 +67,7 @@ const BarGauge = (props: Props) => {
 
                     const metrics = measureText(v.title, titleSize, 500)
                     const titleHeight = metrics.fontBoundingBoxAscent + metrics.fontBoundingBoxDescent;
-                    const lcdSize = orientation == "horizontal" ? width - textWidth - 20 : height - titleHeight - 20
+                    const lcdSize = orientation == "horizontal" ? width - textWidth - 20 : height - titleHeight - 20 - ((showMin || showMax) ? minMaxHeight : 0)
                     const lcdCellCount = Math.floor(lcdSize / lcdCellWidth!);
                     const lcdCellSize = Math.floor((lcdSize - lcdCellSpacing * lcdCellCount) / lcdCellCount);
                     const thresholds = v.thresholds ?? props.threshods
@@ -82,7 +83,7 @@ const BarGauge = (props: Props) => {
                                                 <HStack spacing={`${lcdCellSpacing}px`} height="100%">
                                                     {
                                                         Array.from({ length: lcdCellCount }, (_, i) => {
-                                                            const threshold = getThreshold(v.min + ((i + 1) / lcdCellCount) * (v.max - v.min),thresholds, v.max)
+                                                            const threshold = getThreshold(v.min + ((i + 1) / lcdCellCount) * (v.max - v.min), thresholds, v.max)
                                                             const cellColor = i < Math.floor((v.value - v.min) * lcdCellCount / (v.max - v.min)) ? `radial-gradient(${alpha(threshold?.color ?? v.color, 0.95)} 10%, ${alpha(threshold?.color ?? v.color, 0.55)} )` : alpha(threshold?.color ?? v.color, 0.25);
                                                             return <Box key={i} width={`${lcdCellSize}px`} height="100%" bg={cellColor} borderRadius="2px"></Box>
                                                         })
@@ -92,22 +93,18 @@ const BarGauge = (props: Props) => {
                                                 <Box bg={color == "transparent" ? color : alpha(color, fillOpacity)} width={`${Math.min((v.value - v.min) * 100 / (v.max - v.min), 100)}%`} height="100%" borderRadius={borderRadius} borderRight={`2px solid ${color}`}></Box>}
                                         </Box>
                                         <Box width={textWidth} maxW="40%" >
-                                            {showMin && <Text fontSize="0.6rem" opacity="0.4">{`${formatUnit(v.min,v.units,v.decimal)}`}</Text>}
+                                            {showMin && <Text fontSize="0.6rem" opacity="0.4">{`${formatUnit(v.min, v.units, v.decimal)}`}</Text>}
                                             <Text fontSize={`${textSize}px`} color={color}>{v.text}</Text>
-                                            {showMax && <Text fontSize="0.6rem" opacity="0.4">{`${formatUnit(v.max,v.units,v.decimal)}`}</Text>}
+                                            {showMax && <Text fontSize="0.6rem" opacity="0.4">{`${formatUnit(v.max, v.units, v.decimal)}`}</Text>}
                                         </Box>
                                     </Flex>
                                 </>
                             }
                         </Box>
                     } else {
-                        return <Box width={`${width / data.length}px`} height="100%" textAlign="center">
-                            <Flex justifyContent="space-between" alignItems="center">
-                            { showMin ? <Text fontSize="0.6rem" opacity="0.4">{`${formatUnit(v.min,v.units,v.decimal)}`} </Text> : <Box></Box>}
-                                <Text fontSize={`${textSize}px`} color={color} >{v.text}</Text>
-                                {showMax ? <Text fontSize="0.6rem" opacity="0.4"> {showMax && `${formatUnit(v.max,v.units,v.decimal)}`}</Text> : <Box></Box>}
-                            </Flex>
-                            <Box height={`calc(100% - ${titleHeight}px - 30px)`} width="100%" bg={showUnfilled ? useColorModeValue("rgb(244, 245, 245)", "rgba(255,255,255,0.1)") : null} position="relative">
+                        return <Box width={`${width / data.length}px`} height={height} textAlign="center">
+                            <Text lineHeight={`${titleHeight}px`} fontSize={`${textSize}px`} color={color} noOfLines={1}>{v.text}</Text>
+                            <Box height={`calc(100% - ${titleHeight + ((showMin || showMax) ? minMaxHeight : 0)}px - 30px)`} width="100%" bg={showUnfilled ? useColorModeValue("rgb(244, 245, 245)", "rgba(255,255,255,0.1)") : null} position="relative">
                                 {mode == "lcd" ?
                                     <VStack width="100%" alignItems="left">
                                         {
@@ -121,6 +118,10 @@ const BarGauge = (props: Props) => {
                                     :
                                     <Box position="absolute" bottom="0" bg={color == "transparent" ? color : alpha(color, fillOpacity)} height={`${(v.value - v.min) * 100 / (v.max - v.min)}%`} width="100%" borderRadius={borderRadius} borderTop={`2px solid ${color}`}></Box>}
                             </Box>
+                            <Flex justifyContent="space-between" alignItems="center">
+                                {showMin ? <Text fontSize="0.6rem" opacity="0.4" noOfLines={1}>{`${formatUnit(v.min, v.units, v.decimal)}`} </Text> : <Box></Box>}
+                                {showMax ? <Text fontSize="0.6rem" opacity="0.4" noOfLines={1}> {showMax && `${formatUnit(v.max, v.units, v.decimal)}`}</Text> : <Box></Box>}
+                            </Flex>
                             {v.title && <Box height={`${titleHeight}px`}><Text fontSize={`${titleSize}px`} fontWeight={500}>{v.title}</Text></Box>}
                         </Box>
                     }
