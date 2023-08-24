4c95061786c878c715f133ac3f741b217fd02140
sunface <cto@188.com>
2023-08-24 14:25:56 +0800

    docs: add query variable to tutorial

---
+++ website/src/components/docs/Code.js
@@ -0,0 +1,5 @@
+const Code  = ({children}) => {
+    return <span className="bg-sky-400/20 font-medium text-sky-600 dark:bg-[#7dd3fc1a] dark:text-[rgb(125,211,252)] rounded px-[4px] py-[3px] text-sm" >{children}</span>
+}
+
+export default Code
