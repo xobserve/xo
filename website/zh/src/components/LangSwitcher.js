ced6970ed223513f13b7cd5d9fd85cef4a38525d
sunface <cto@188.com>
2023-09-19 18:06:28 +0800

    fix: lang switcher in website open a browser tab

--- website/zh/src/components/LangSwitcher.js
+++ website/zh/src/components/LangSwitcher.js
@@ -1,5 +1,5 @@
 export const LangSwitcher = () => {
-    return (<span className="cursor-pointer w-12 text-sm font-medium" onClick={() => window.open("https://datav.io" + location.pathname)}>
+    return (<span className="cursor-pointer w-12 text-sm font-medium" onClick={() => location.href = ("https://datav.io" + location.pathname)}>
         <p>EN</p>
     </span>)
 }
