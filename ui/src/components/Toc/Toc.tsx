f43331009a7b9117e8f8996c16c5563b6547e02e
sunface <cto@188.com>
2023-09-27 19:34:30 +0800

    chore: update toc padding in datasource docs

--- ui/src/components/Toc/Toc.tsx
+++ ui/src/components/Toc/Toc.tsx
@@ -8,12 +8,12 @@ const Toc = ({toc}) => {
     const [activeId, setActiveId] = useState(null);
     useIntersectionObserver('.markdown-render',setActiveId);
 
-    return <VStack alignItems="left"  background={useColorModeValue(customColors.primaryColor.light, customColors.bodyBg.dark)}  py="2" maxH="100vh" overflowY="auto">
+    return <VStack spacing={0} alignItems="left"  background={useColorModeValue(customColors.primaryColor.light, customColors.bodyBg.dark)}   maxH="100vh" overflowY="auto">
     {
         toc.map(t => {
             const level = Number(t.level)
             const id = t.content.toLowerCase().replace(/[?\s]/g, "-")
-            return <Heading py="2px" px="2" className={activeId == id ? "label-bg" : null}  pl={15 * level + 'px'} fontSize="0.9rem" color={useColorModeValue(activeId == id ?  "inherit" : "white", "brand.500" )} cursor="pointer" onClick={() => {
+            return <Heading py="2" px="2" className={activeId == id ? "label-bg" : null}  pl={15 * level + 'px'} fontSize="0.9rem" color={useColorModeValue(activeId == id ?  "inherit" : "white", "brand.500" )} cursor="pointer" onClick={() => {
                 const el = document.getElementById(id)
                 if (el) {
                     el.scrollIntoView({ behavior: "smooth", block: "center" })
